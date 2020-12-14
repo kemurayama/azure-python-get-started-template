c5727362373e87e04ec3d8a112419911f5e3da58
kemurayama <kemura@microsoft.com>
2020-12-14 21:59:41 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# azure-python-get-started-template
+# Azure Python Get Started Template
+
+This repository deploys [Azure Web Apps](https://azure.microsoft.com/en-us/services/app-service/web/) and [Function App] (https://azure.microsoft.com/en-us/services/functions/)
+
+
+
+## LICENSE
+
+[MIT LICENSE](./LICENSE)
---
+++ terraform/vairables.tf
@@ -0,0 +1,23 @@
+variable "resource_group" {
+  type    = string
+  default = "rg-terraform"
+}
+
+variable "prefix" {
+  type    = string
+  default = "terraform"
+}
+
+variable "location" {
+  type    = string
+  default = "japaneast"
+}
+
+#variable "azure_client_id" {
+#  type = string
+#}
+#
+#variable "azure_client_secret" {
+#  type = string
+#}
+
